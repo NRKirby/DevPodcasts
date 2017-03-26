5ecdc04086d833aaf42851dff9b03b06c5214e8e
Nick Kirby <nrkirb@gmail.com>
2017-03-26 17:17:56 +0100

    Add project files.

---
+++ DevPodcasts.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DevPodcasts.Web.MvcApplication" Language="C#" %>
---
+++ DevPodcasts.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DevPodcasts.Web.Startup))]
+namespace DevPodcasts.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
