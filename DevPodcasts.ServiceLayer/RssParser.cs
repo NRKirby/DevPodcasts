7fc08ece12ce750c1e766dbc0f190f80991cf842
Nick Kirby <nrkirb@gmail.com>
2017-04-14 06:14:32 +0100

    Comment out code for parsing dates that are causing an exception

--- DevPodcasts.ServiceLayer/RssParser.cs
+++ DevPodcasts.ServiceLayer/RssParser.cs
@@ -1,4 +1,8 @@
-﻿using System.ServiceModel.Syndication;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.ServiceModel.Syndication;
+using System.Text;
 using System.Xml;
 
 namespace DevPodcasts.ServiceLayer
@@ -7,6 +11,17 @@ namespace DevPodcasts.ServiceLayer
     {
         public SyndicationFeed ParseRssFeed(string rssFeedUrl)
         {
+            //string xml; TODO (http://developeronfire.com/rss.xml)
+            //using (WebClient webClient = new WebClient())
+            //{
+            //    xml = Encoding.UTF8.GetString(webClient.DownloadData(rssFeedUrl));
+            //}
+            //xml = xml.Replace("2017-04-13", "Thu, 13 Apr 2017");
+            //byte[] bytes = Encoding.ASCII.GetBytes(xml);
+            //XmlReader reader = XmlReader.Create(new MemoryStream(bytes));
+            //SyndicationFeed feed2 = SyndicationFeed.Load(reader);
+
+
             var xmlReader = XmlReader.Create(rssFeedUrl);
             var feed = SyndicationFeed.Load(xmlReader);
 
