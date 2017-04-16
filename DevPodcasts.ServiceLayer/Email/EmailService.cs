22df8c2ad65b2d10e74108876d5ee624db15fac8
Nick Kirby <nrkirb@gmail.com>
2017-04-16 11:20:05 +0100

    Hook up Send Grid for registration confirmation

---
+++ DevPodcasts.ServiceLayer/Email/EmailService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System.Threading.Tasks;
+
+namespace DevPodcasts.ServiceLayer.Email
+{
+    public class EmailService : IIdentityMessageService
+    {
+        public async Task SendAsync(IdentityMessage message)
+        {
+            await SendWithSendGridAsync(message);
+        }
+
+        private async Task SendWithSendGridAsync(IdentityMessage message)
+        {
+            var apiKey = "";
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress("no-reply@devpodcasts.net", "Dev Podcasts");
+            var bcc = new EmailAddress("nrkirb@gmail.com");
+            var msg = new SendGridMessage()
+            {
+                From = from,
+                Subject = message.Subject,
+                HtmlContent = message.Body
+            };
+            msg.AddTo(new EmailAddress(message.Destination));
+            //msg.AddBcc(bcc);
+            var response = await client.SendEmailAsync(msg);
+        }
+    }
+}
--- DevPodcasts.Web/App_Start/IdentityConfig.cs
+++ DevPodcasts.Web/App_Start/IdentityConfig.cs
@@ -1,24 +1,16 @@
-﻿using System;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using DevPodcasts.DataLayer.Models;
+using DevPodcasts.ServiceLayer.Email;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using DevPodcasts.DataLayer.Models;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace DevPodcasts.Web
 {
-    public class EmailService : IIdentityMessageService
-    {
-        public Task SendAsync(IdentityMessage message)
-        {
-            // Plug in your email service here to send an email.
-            return Task.FromResult(0);
-        }
-    }
-
     public class SmsService : IIdentityMessageService
     {
         public Task SendAsync(IdentityMessage message)
