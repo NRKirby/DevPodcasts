f055feb87e4351d24e0ba3dd2850f8f3622c11e6
Nick Kirby <nrkirb@gmail.com>
2017-04-18 14:46:43 +0100

    Tidy up

--- DevPodcasts.ServiceLayer/RssParser.cs
+++ DevPodcasts.ServiceLayer/RssParser.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using System.ServiceModel.Syndication;
-using System.Text;
+﻿using System.ServiceModel.Syndication;
 using System.Xml;
 
 namespace DevPodcasts.ServiceLayer
@@ -11,17 +7,6 @@ namespace DevPodcasts.ServiceLayer
     {
         public SyndicationFeed ParseRssFeed(string rssFeedUrl)
         {
-            //string xml; TODO (http://developeronfire.com/rss.xml)
-            //using (WebClient webClient = new WebClient())
-            //{
-            //    xml = Encoding.UTF8.GetString(webClient.DownloadData(rssFeedUrl));
-            //}
-            //xml = xml.Replace("2017-04-13", "Thu, 13 Apr 2017");
-            //byte[] bytes = Encoding.ASCII.GetBytes(xml);
-            //XmlReader reader = XmlReader.Create(new MemoryStream(bytes));
-            //SyndicationFeed feed2 = SyndicationFeed.Load(reader);
-
-
             var xmlReader = XmlReader.Create(rssFeedUrl);
             var feed = SyndicationFeed.Load(xmlReader);
 
