d4fa33b71cd13de092bd5dd5cdfe21b6aaba7c84
Nick Kirby <nrkirb@gmail.com>
2017-04-21 16:00:27 +0100

    Begin working on search in navbar

---
+++ DevPodcasts.Web/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DevPodcasts.Web.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Index(string query)
+        {
+            return View();
+        }
+    }
+}
