23fbadbdd166d167419143d8ad2b356c672212a4
Nick Kirby <nrkirb@gmail.com>
2017-04-22 13:20:21 +0100

    Factor out log file string to web.config

--- DevPodcasts.ServiceLayer/Logging/FileLogger.cs
+++ DevPodcasts.ServiceLayer/Logging/FileLogger.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.IO;
+using System.Web.Configuration;
 
 namespace DevPodcasts.ServiceLayer.Logging
 {
     public class FileLogger : ILogger
     {
+        private readonly string _logFilePath = WebConfigurationManager.AppSettings["LogFilePath"];
+
         public void Error(Exception ex)
         {
-            using (var sw = new StreamWriter(@"C:\Users\Nick\Desktop\projects\DevPodcasts\DevPodcasts.ServiceLayer\Logging\Data\Log.txt", true))
+            using (var sw = new StreamWriter(_logFilePath, true))
             {
                 sw.WriteLine(DateTime.Now + " Error: " + ex.Message);
             }
@@ -15,7 +18,7 @@ namespace DevPodcasts.ServiceLayer.Logging
 
         public void Info(object msg)
         {
-            using (var sw = new StreamWriter(@"C:\Users\Nick\Desktop\projects\DevPodcasts\DevPodcasts.ServiceLayer\Logging\Data\Log.txt", true))
+            using (var sw = new StreamWriter(_logFilePath, true))
             {
                 sw.WriteLine(DateTime.Now + " Info: " + msg);
             }
@@ -23,7 +26,7 @@ namespace DevPodcasts.ServiceLayer.Logging
 
         public void Debug(string msg)
         {
-            using (var sw = new StreamWriter(@"C:\Users\Nick\Desktop\projects\DevPodcasts\DevPodcasts.ServiceLayer\Logging\Data\Log.txt", true))
+            using (var sw = new StreamWriter(_logFilePath, true))
             {
                 sw.WriteLine(DateTime.Now + " Debug: " + msg);
             }
@@ -31,7 +34,7 @@ namespace DevPodcasts.ServiceLayer.Logging
 
         public void Error(string msg, Exception ex)
         {
-            using (var sw = new StreamWriter(@"C:\Users\Nick\Desktop\projects\DevPodcasts\DevPodcasts.ServiceLayer\Logging\Data\Log.txt", true))
+            using (var sw = new StreamWriter(_logFilePath, true))
             {
                 sw.WriteLine(DateTime.Now + " Error: "+ msg + ex.Message);
             }
