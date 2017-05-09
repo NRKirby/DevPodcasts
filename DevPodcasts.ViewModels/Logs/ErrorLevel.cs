0acbbcd313f376777ac3fb0add00ea12486de9f2
Nick Kirby <nrkirb@gmail.com>
2017-05-09 17:25:45 +0100

    Work on logs table, not quite there...

---
+++ DevPodcasts.ViewModels/Logs/ErrorLevel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace DevPodcasts.ViewModels.Logs
+{
+    public enum ErrorLevel
+    {
+        [Description("Debug")]
+        Debug = 0,
+        [Description("Error")]
+        Error = 1,
+        [Description("Info")]
+        Info = 2
+    }
+}
