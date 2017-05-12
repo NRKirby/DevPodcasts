055603c23456ba0f420bd751ec694f39c3016e4d
Nick Kirby <nick@remote.online>
2017-05-12 13:26:24 +0100

    Move updater code to console project

---
+++ DevPodcasts.EpisodeUpdater/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DevPodcasts.EpisodeUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var updater = DependencyResolver.Current.GetService<EpisodeUpdater>();
+            updater.Update();
+        }
+    }
+}
