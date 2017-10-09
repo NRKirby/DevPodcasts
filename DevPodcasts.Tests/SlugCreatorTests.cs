f4b099f798d3d894effa55d20da6e65767e71e1f
Nick Kirby <nick@remote.online>
2017-10-09 13:55:45 +0100

    Begin writing tests for slug creator

---
+++ DevPodcasts.Tests/SlugCreatorTests.cs
@@ -0,0 +1,21 @@
+﻿using DevPodcasts.ServiceLayer.Podcast;
+using Shouldly;
+using Xunit;
+
+namespace DevPodcasts.Tests
+{
+    public class SlugCreatorTests
+    {
+        [Fact]
+        public void SoftwareEngineeringDailyShouldReturnCorrectSlug()
+        {
+            var sut = new SlugCreator();
+            const string input = "Software Engineering Daily";
+            const string expected = "software-engineering-daily";
+
+            var result = sut.Create(input);
+
+            result.ShouldBe(expected);
+        }
+    }
+}
