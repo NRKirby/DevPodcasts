7e2c2e836d2b9c2fe4e2b59e5d27b6424c5686f0
Nick Kirby <nrkirb@gmail.com>
2017-10-17 06:59:22 +0100

    Begin working on fixing episode summary issue

---
+++ test/DevPodcasts.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DevPodcasts.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DevPodcasts.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ce17c881-26cf-4292-a97b-7d56b0853355")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/DevPodcasts.Tests/RssParserTest.cs
@@ -0,0 +1,24 @@
+﻿using DevPodcasts.Logging;
+using DevPodcasts.Models;
+using DevPodcasts.ServiceLayer.RSS;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevPodcasts.Tests
+{
+    [TestClass]
+    public class RssParserTest
+    {
+        [TestMethod]
+        public void DoubleYourFreelancingPodcast_EpisodeSummaryLocationShouldBeContent()
+        {
+            var fakeLogger = A.Fake<ILogger>();
+            var sut = new RssParser(fakeLogger);
+            const string feedUrl = "https://rss.simplecast.com/podcasts/219/rss";
+
+            var result = sut.ParseRssFeed(feedUrl);
+
+            Assert.AreEqual(EpisodeSummaryLocation.Content, result.EpisodeSummaryLocation);
+        }
+    }
+}
