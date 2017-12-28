27adde60d6b08aa9d26b9bac1d9ce64a0c1d0ae6
Nick Kirby <nrkirb@gmail.com>
2017-12-28 19:09:21 +0000

    More model changes.

--- src/DevPodcasts.DataLayer/Models/ApplicationUser.cs
+++ src/DevPodcasts.DataLayer/Models/ApplicationUser.cs
@@ -20,8 +20,6 @@ namespace DevPodcasts.DataLayer.Models
 
         public string LastName { get; set; }
 
-        public virtual ICollection<Podcast> SubscribedPodcasts { get; set; }
-
         public virtual ICollection<LibraryPodcast> LibraryPodcasts { get; set; }
 
         public virtual ICollection<LibraryEpisode> LibraryEpisodes { get; set; }
