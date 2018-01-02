a64649c30aae674b9739781cbbf6fdd463258ad5
NRKirby <nrkirb@gmail.com>
2018-01-02 19:23:03 +0000

    Use DataLayer0.0.2 package etc.

--- src/DevPodcasts.NotifyPodcastSubscribers/Function.cs
+++ src/DevPodcasts.NotifyPodcastSubscribers/Function.cs
@@ -1,11 +1,11 @@
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Threading.Tasks;
 using DevPodcasts.DataLayer.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace DevPodcasts.NotifyPodcastSubscribers
 {
@@ -31,16 +31,13 @@ namespace DevPodcasts.NotifyPodcastSubscribers
                 .FirstOrDefault(q => string.Compare(q.Key, "podcastId", true) == 0)
                 .Value);
 
-            const string connectionString = "Server=tcp:devpodcasts.database.windows.net,1433;Initial Catalog=devpodcasts;Persist Security Info=False;User ID=whiffwhaff9238;Password=mtisaIr2ESthVS64Kx7z;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            //const string connectionString = "Server=tcp:devpodcasts.database.windows.net,1433;Initial Catalog=devpodcasts;Persist Security Info=False;User ID=whiffwhaff9238;Password=mtisaIr2ESthVS64Kx7z;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=DevPodcasts02012018;Integrated Security=True";
             var context = new ApplicationDbContext(connectionString);
 
             var users = context.LibraryPodcasts.Where(p => p.PodcastId == podcastId).Select(p => p.ApplicationUser).ToList();
 
-
             // notify each user of new episode
-
-
-
 
             return req.CreateResponse(HttpStatusCode.OK, "Hello " + key);
         }
