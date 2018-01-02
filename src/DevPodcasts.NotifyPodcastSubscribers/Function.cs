a81c168db1ea17db9c1b0f3964e239ec353f08d6
Nick Kirby <nrkirb@gmail.com>
2018-01-02 20:17:03 +0000

    Converting NotifyPodcastSubscribers to POST request

--- src/DevPodcasts.NotifyPodcastSubscribers/Function.cs
+++ src/DevPodcasts.NotifyPodcastSubscribers/Function.cs
@@ -12,34 +12,35 @@ namespace DevPodcasts.NotifyPodcastSubscribers
     public static class Function
     {
         [FunctionName("NotifyPodcastSubscribers")] // http://localhost:7071/api/NotifyPodcastSubscribers?key=theKey&episodeId=22&podcastId=141
-        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)]HttpRequestMessage req, TraceWriter log)
+        public static async Task<HttpResponseMessage> Run([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)]HttpRequestMessage req, TraceWriter log)
         {
             log.Info("C# HTTP trigger function processed a request.");
 
-            string key = req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "key", true) == 0)
-                .Value;
-
-            if (key != "theKey")
-                return req.CreateResponse(HttpStatusCode.BadRequest, "Invalid key");
-
-            int episodeId = int.Parse(req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "episodeId", true) == 0)
-                .Value);
-
-            int podcastId = int.Parse(req.GetQueryNameValuePairs()
-                .FirstOrDefault(q => string.Compare(q.Key, "podcastId", true) == 0)
-                .Value);
+            PostData data = await req.Content.ReadAsAsync<PostData>();
 
             //const string connectionString = "Server=tcp:devpodcasts.database.windows.net,1433;Initial Catalog=devpodcasts;Persist Security Info=False;User ID=whiffwhaff9238;Password=mtisaIr2ESthVS64Kx7z;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             const string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=DevPodcasts02012018;Integrated Security=True";
             var context = new ApplicationDbContext(connectionString);
 
+
+            var key = data?.Key;
+            int episodeId = data.EpisodeId;
+            var podcastId = context.Episodes.Where(episode => episode.Id == episodeId).Select(episode => episode.Podcast.Id).SingleOrDefault();
+
+            if (key != "theKey")
+                return req.CreateResponse(HttpStatusCode.BadRequest, "Invalid key");
+
             var users = context.LibraryPodcasts.Where(p => p.PodcastId == podcastId).Select(p => p.ApplicationUser).ToList();
 
             // notify each user of new episode
 
-            return req.CreateResponse(HttpStatusCode.OK, "Hello " + key);
+            return req.CreateResponse(HttpStatusCode.OK, "Hello ");
         }
+    }
+
+    public class PostData
+    {
+        public string Key { get; set; }
+        public int EpisodeId { get; set; }
     }
 }
