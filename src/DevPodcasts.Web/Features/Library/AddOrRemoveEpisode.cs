a23b26ce5f229b2202af6167b245cdfcb645a137
NRKirby <nrkirb@gmail.com>
2018-01-14 09:32:09 +0000

    Handler for adding / removing episode from library.

--- src/DevPodcasts.Web/Features/Library/AddOrRemoveEpisode.cs
+++ src/DevPodcasts.Web/Features/Library/AddOrRemoveEpisode.cs
@@ -1,5 +1,8 @@
 ﻿using DevPodcasts.DataLayer.Models;
 using MediatR;
+using System;
+using System.Data.Entity;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DevPodcasts.Web.Features.Library
@@ -23,7 +26,48 @@ namespace DevPodcasts.Web.Features.Library
 
             public async Task<AddRemoveLibraryAjaxModel> Handle(Command message)
             {
-                return null;
+                var model = new AddRemoveLibraryAjaxModel { IsSuccess = false };
+                var user = await _context.Users.SingleOrDefaultAsync(u => u.Id == message.UserId);
+                if (user == null)
+                {
+                    model.Error = "User doesn't exist";
+                    return model;
+                }
+
+                var episode = await _context.Episodes.SingleOrDefaultAsync(p => p.Id == message.EpisodeId);
+                if (episode == null)
+                {
+                    model.Error = "Episode doesn't exist";
+                    return model;
+                }
+
+                var userIsSubscribed = user.LibraryEpisodes.Any(p => p.EpisodeId == episode.Id);
+                if (userIsSubscribed)
+                {
+                    var libraryEpisode = await _context.LibraryEpisodes.SingleAsync(e => e.EpisodeId == episode.Id);
+                    _context.LibraryEpisodes.Remove(libraryEpisode);
+                    user.LibraryEpisodes.Remove(libraryEpisode);
+                    model.IsAdded = false;
+                }
+                else
+                {
+                    var libraryEpisode = new LibraryEpisode
+                    {
+                        EpisodeId = episode.Id,
+                        Episode = episode,
+                        UserId = user.Id,
+                        ApplicationUser = user,
+                        DateAdded = DateTime.Now
+                    };
+                    user.LibraryEpisodes.Add(libraryEpisode);
+                    _context.LibraryEpisodes.Add(libraryEpisode);
+                    model.IsAdded = true;
+                }
+
+                model.IsSuccess = true;
+                await _context.SaveChangesAsync();
+
+                return model;
             }
         }
     }
