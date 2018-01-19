1b491cd61481ef40b5f1e4f9b34711ec74566416
Nick Kirby <nick@remote.online>
2018-01-19 08:56:47 +0000

    Begin fleshing out sitemap as per how generated in Umbraco.

---
+++ src/DevPodcasts.Web/Controllers/SitemapController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DevPodcasts.Web.Controllers
+{
+    public class SitemapController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
